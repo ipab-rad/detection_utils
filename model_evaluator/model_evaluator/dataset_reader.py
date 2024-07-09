9a8ea68216f0ef634c7d6ff6268a7604d2df430d
Julius Schulte <jschulte.oedt@t-online.de>
2024-07-09 14:27:15 +0100

    make interface classes abstract

--- model_evaluator/model_evaluator/dataset_reader.py
+++ model_evaluator/model_evaluator/dataset_reader.py
@@ -1,7 +1,8 @@
+from abc import ABC, abstractmethod
 import numpy as np
 
-class ImageDatasetReader:
+class ImageDatasetReader(ABC):
     # TODO: use generator
-
+    @abstractmethod
     def readImages(self) -> list[np.ndarray]:
-        pass
+        raise NotImplementedError
--- model_evaluator/model_evaluator/inference_connector.py
+++ model_evaluator/model_evaluator/inference_connector.py
@@ -1,9 +1,10 @@
+from abc import ABC, abstractmethod
 import numpy as np
 from typing import Optional
 
 from model_evaluator.detection import Detection
 
-class InferenceConnector:
-
+class InferenceConnector(ABC):
+    @abstractmethod
     def runInference(self, image: np.ndarray) -> Optional[list[Detection]]:
-        pass
+        raise NotImplementedError
