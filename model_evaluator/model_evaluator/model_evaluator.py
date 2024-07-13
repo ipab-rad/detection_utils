5a3115fcbd397c74f3a9ca662d689f250f401667
Julius Schulte <jschulte.oedt@t-online.de>
2024-07-13 15:47:04 +0100

    Added Waymo reader and evaluation

--- model_evaluator/model_evaluator/model_evaluator.py
+++ model_evaluator/model_evaluator/model_evaluator.py
@@ -1,47 +1,115 @@
-from model_evaluator.model_evaluator.detection import BBox, Detection
+from model_evaluator.waymo_reader import WaymoDatasetReader2D
+from model_evaluator.detection import BBox2D, Label2D
 from model_evaluator.yolox_connector import TensorrtYOLOXConnector
 import cv2
 import numpy as np
 
-def iou(bbox1: BBox, bbox2: BBox):
-    # intersection rect
-    left = max(bbox1.x1, bbox2.x1)
-    top = max(bbox1.y1, bbox2.y1)
-    right = min(bbox1.x2, bbox2.x2)
-    bottom = min(bbox1.y2, bbox2.y2)
+def calculate_ious_2d(pred_bboxes: list[BBox2D], gt_bboxes: list[BBox2D]) -> np.ndarray:
+    ious = np.empty((len(pred_bboxes), len(gt_bboxes)))
 
-    if right < left or bottom < top:
-        return 0.0
+    for i, pred_bbox in enumerate(pred_bboxes):
+        for j, gt_bbox in enumerate(gt_bboxes):
+            ious[i, j] = pred_bbox.iou(gt_bbox)
+
+    return ious
+
+def get_tp_fp(ious: np.ndarray, threshold: float) -> tuple[np.ndarray, np.ndarray]:
+    num_detections = ious.shape[0]
+    num_gts = ious.shape[1]
+
+    tp = np.zeros(num_detections)
+    fp = np.zeros(num_detections)
+
+    matched_gts = []
+
+    for i in range(num_detections):
+        max_iou = 0
+        max_iou_idx = -1
+
+        for j in range(num_gts):
+            iou = ious[i, j]
+
+            if iou > max_iou:
+                max_iou = iou
+                max_iou_idx = j
+
+        if max_iou >= threshold:
+            if max_iou_idx not in matched_gts:
+                tp[i] = 1
+                matched_gts.append(max_iou_idx)
+            else:
+                fp[i] = 1
+        else:
+            fp[i] = 1
+
+    return tp, fp
+
+def calculate_ap(tp, fp, num_samples):
+    tp_cumsum = np.cumsum(tp)
+    fp_cumsum = np.cumsum(fp)
+
+    precisions = tp_cumsum / (tp_cumsum + fp_cumsum)
+    recalls = tp_cumsum / num_samples
+
+    precisions = np.concatenate(([0], precisions, [0]))
+    recalls = np.concatenate(([0], recalls, [1]))
+
+    for i in range(len(precisions) - 1, 0, -1):
+        precisions[i - 1] = max(precisions[i - 1], precisions[i])
+
+    indices = np.where(recalls[1:] != recalls[:-1])[0]
+
+    return np.sum((recalls[indices + 1] - recalls[indices]) * precisions[indices + 1])
+
     
-    intersect_area = (right - left) * (bottom - top)
+def to_cv_pts(bbox: BBox2D):
+    return (round(bbox.x1), round(bbox.y1)), (round(bbox.x2), round(bbox.y2))
 
-    return intersect_area / (bbox1.area() + bbox2.area() - intersect_area)
+def draw_bboxes(image, gts, detections, included_labels=Label2D.ALL):
+    for gt in gts:
+        if gt.label not in included_labels:
+            continue
 
-def calculate_metrics(pred_bboxes: list[BBox], gt_bboxes: list[BBox]):
-    res = np.zeros(len(pred_bboxes))
+        pt1, pt2 = to_cv_pts(gt.bbox)
+        cv2.rectangle(image, pt1, pt2, (0, 0, 255), 2)
 
-    for i, pred in enumerate(pred_bboxes):
-        for gt in gt_bboxes:
-            iou = iou(pred, gt)
+        cv2.putText(image, str(gt.label), (pt1[0], pt1[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,0,255), 2)
 
-            if iou > 0.5:
-                res[i] = 1
+    for detection in detections:
+        if detection.label not in included_labels:
+            continue
 
-                break
+        pt1, pt2 = to_cv_pts(detection.bbox)
+        cv2.rectangle(image, pt1, pt2, (0, 255, 0), 2)
 
-    acc_tp = sum(res)
-    acc_fp = len(res) - sum(res)
-
-    recall = acc_tp / len(gt_bboxes)
-    precision = acc_tp / (acc_tp + acc_fp)
-
+        cv2.putText(image, str(detection.label), (pt1[0], pt1[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
 
 def main():
     connector = TensorrtYOLOXConnector('/sensor/camera/lspf_r/image_raw', '/perception/object_recognition/detection/rois0')
 
-    img = cv2.imread('<IMAGE PATH>')
+    reader = WaymoDatasetReader2D()
 
-    detections = connector.runInference(img)
+    data = reader.read_data()
 
-    for detection in detections:
-        print(detection)
+    for i, (image, gts) in enumerate(data):
+        detections = connector.runInference(image)
+        draw_bboxes(image, gts, detections, Label2D.ALL)
+        cv2.imwrite(f'image{i}.png', image)
+
+        aps = {}
+
+        for label in [Label2D.PEDESTRIAN, Label2D.BICYCLE, Label2D.VEHICLE]:
+            label_gts = [gt for gt in gts if gt.label in label]
+            label_detections = [detection for detection in detections if detection.label in label]
+
+            label_detections.sort(key=lambda x: x.score, reverse=True)
+
+            ious = calculate_ious_2d(label_detections, label_gts)
+
+            tp, fp = get_tp_fp(ious, 0.5)
+
+            aps[label] = calculate_ap(tp, fp, len(label_gts))
+
+        mAP = np.mean(list(aps.values()))
+
+        print(f'{mAP=}')
---
+++ model_evaluator/model_evaluator/waymo_reader.py
@@ -0,0 +1,105 @@
+import numpy as np
+import glob
+import dask.dataframe as dd
+from waymo_open_dataset import v2, label_pb2
+import cv2
+
+from model_evaluator.dataset_reader import DatasetReader2D
+from model_evaluator.detection import Detection2D, BBox2D, Label2D
+
+dataset_dir = '/rosbags/waymo/validation'
+
+contexts = [
+    '1024360143612057520_3580_000_3600_000,1553735853462203',
+    '1024360143612057520_3580_000_3600_000,1553735853662172'
+]
+
+def read(tag, context_name):
+    paths = glob.glob(f'{dataset_dir}/{tag}/{context_name}.parquet')
+
+    return dd.read_parquet(paths)
+
+def parse_context_names_and_timestamps():
+    lines = contexts
+
+    context_names = {}
+    for line in lines:
+        context_name, timestamp = line.split(',')
+
+        timestamp = int(timestamp)
+
+        if context_name not in context_names:
+            context_names[context_name] = [timestamp]
+        else:
+            context_names[context_name].append(timestamp)
+
+    return context_names
+
+def parse_waymo_label(label: int) -> Label2D:
+   match label:
+      case label_pb2.Label.TYPE_VEHICLE:
+         return Label2D.VEHICLE
+      case label_pb2.Label.TYPE_PEDESTRIAN:
+         return Label2D.PEDESTRIAN
+      case label_pb2.Label.TYPE_CYCLIST:
+         return Label2D.BICYCLE
+      case _:
+         return Label2D.UNKNOWN
+
+class WaymoDatasetReader2D(DatasetReader2D):
+    @staticmethod
+    def decode_image(image_component: v2.CameraImageComponent) -> np.ndarray:
+        return cv2.imdecode(np.frombuffer(image_component.image, dtype=np.uint8), cv2.IMREAD_COLOR)
+    
+    @staticmethod
+    def decode_label(label: int) -> Label2D:
+        match label:
+            case label_pb2.Label.TYPE_VEHICLE:
+                return Label2D.VEHICLE
+            case label_pb2.Label.TYPE_PEDESTRIAN:
+                return Label2D.PEDESTRIAN
+            case label_pb2.Label.TYPE_CYCLIST:
+                return Label2D.BICYCLE
+            case _:
+                return Label2D.UNKNOWN
+    
+    @staticmethod
+    def decode_detections(box_component: v2.CameraBoxComponent) -> list[Detection2D]:
+        detections = []
+
+        for cx, cy, w, h, type in zip(box_component.box.center.x,
+                                      box_component.box.center.y,
+                                      box_component.box.size.x,
+                                      box_component.box.size.y,
+                                      box_component.type):
+            bbox = BBox2D.from_cxcywh(cx, cy, w, h)
+            label = WaymoDatasetReader2D.decode_label(type)
+
+            detections.append(Detection2D(bbox, 1.0, label))
+
+        return detections
+    
+    def read_data(self) -> list[tuple[np.ndarray, list[Detection2D]]]:
+        context_names = parse_context_names_and_timestamps()
+
+        output = []
+
+        for context_name in context_names:
+            cam_image_df = read('camera_image', context_name)
+            cam_box_df = read('camera_box', context_name)
+
+            image_w_box_df = v2.merge(cam_image_df, cam_box_df, right_group=True)
+
+            for timestamp in context_names[context_name]:
+                frame_df = image_w_box_df.loc[image_w_box_df['key.frame_timestamp_micros'] == timestamp]
+
+                for (_, r) in frame_df.iterrows():
+                    cam_image = v2.CameraImageComponent.from_dict(r)
+                    cam_box = v2.CameraBoxComponent.from_dict(r)
+
+                    image = self.decode_image(cam_image)
+                    detections = self.decode_detections(cam_box)
+
+                    output.append((image, detections))
+
+        return output
