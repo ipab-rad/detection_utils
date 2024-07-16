fe1c6022fd01a459c51135204af1a0e54b27940b
Julius Schulte <jschulte.oedt@t-online.de>
2024-07-16 11:49:29 +0100

    Improvements to rosbag reader

--- model_evaluator/model_evaluator/rosbag_reader.py
+++ model_evaluator/model_evaluator/rosbag_reader.py
@@ -1,3 +1,5 @@
+from typing import Generator
+
 import numpy as np
 import rosbag2_py
 from sensor_msgs.msg import Image
@@ -10,10 +12,12 @@ from model_evaluator.interfaces.detection2D import Detection2D, BBox2D, Label2D
 
 class RosbagDatasetReader2D(DatasetReader2D):
 
-    def __init__(self, path):
+    def __init__(self, path, expectations: dict[Label2D, int]):
         self.path = path
 
-    def read_data(self) -> list[tuple[np.ndarray, list[Detection2D]]]:
+        self.expectations = expectations
+
+    def read_data(self) -> Generator[tuple[np.ndarray, list[Detection2D]], None, None]:
         reader = rosbag2_py.SequentialReader()
         reader.open(
             rosbag2_py.StorageOptions(uri=self.path, storage_id='mcap'),
@@ -27,29 +31,28 @@ class RosbagDatasetReader2D(DatasetReader2D):
 
         # TODO: Add asserts
 
-        msgs = []
-
         cv_bridge = CvBridge()
 
         while reader.has_next():
-            topic, data, timestamp = reader.read_next()
+            topic, data, _ = reader.read_next()
 
             if topic == '/sensor/camera/fsp_l/image_rect_color':
                 msg = deserialize_message(data, Image)
 
-                msgs.append(
-                    (
-                        cv_bridge.imgmsg_to_cv2(msg),
-                        [
+                image = cv_bridge.imgmsg_to_cv2(msg)
+
+                gts = []
+
+                for label in self.expectations:
+                    for _ in range(self.expectations[label]):
+                        gts.append(
                             Detection2D(
                                 BBox2D.from_xywh(0, 0, msg.width, msg.height),
                                 1.0,
-                                Label2D.PEDESTRIAN,
+                                label,
                             )
-                        ],
-                    )
-                )
+                        )
 
-        return msgs
+                yield image, gts
 
         return None
