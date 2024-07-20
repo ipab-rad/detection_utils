d87cf393570c5e7a25be59658132594cefa2ab87
Julius Schulte <jschulte.oedt@t-online.de>
2024-07-20 16:58:14 +0100

    Differentiate tp and fp in bbox annotations

--- model_evaluator/model_evaluator/utils/cv2_bbox_annotator.py
+++ model_evaluator/model_evaluator/utils/cv2_bbox_annotator.py
@@ -1,44 +1,60 @@
 import cv2
-from model_evaluator.interfaces.detection2D import BBox2D, Label2D
+from model_evaluator.interfaces.detection2D import BBox2D
 
 
 def to_cv_pts(bbox: BBox2D):
     return (round(bbox.x1), round(bbox.y1)), (round(bbox.x2), round(bbox.y2))
 
 
-def draw_bboxes(image, gts, detections, included_labels=Label2D.ALL):
-    for gt in gts:
-        if gt.label not in included_labels:
-            continue
+def draw_bboxes(image, gts, tps, fps):
+    text_height = 15
+    offset = 10
+    thickness = 2
 
+    scale = cv2.getFontScaleFromHeight(
+        cv2.FONT_HERSHEY_SIMPLEX, text_height, thickness
+    )
+
+    for gt in gts:
         pt1, pt2 = to_cv_pts(gt.bbox)
-        cv2.rectangle(image, pt1, pt2, (0, 255, 0), 2)
+        cv2.rectangle(image, pt1, pt2, (255, 0, 0), thickness)
 
         cv2.putText(
             image,
-            str(gt.label),
-            (pt1[0], pt1[1] - 10),
+            str(gt.label.name),
+            (pt1[0], pt1[1] - offset),
             cv2.FONT_HERSHEY_SIMPLEX,
-            0.9,
-            (0, 255, 0),
-            2,
+            scale,
+            (255, 0, 0),
+            thickness,
         )
 
-    for detection in detections:
-        if detection.label not in included_labels:
-            continue
-
-        pt1, pt2 = to_cv_pts(detection.bbox)
-        cv2.rectangle(image, pt1, pt2, (0, 0, 255), 2)
+    for tp in tps:
+        pt1, pt2 = to_cv_pts(tp.bbox)
+        cv2.rectangle(image, pt1, pt2, (0, 255, 0), thickness)
 
         cv2.putText(
             image,
-            str(detection.label),
-            (pt1[0], pt1[1] - 10),
+            str(tp.label.name),
+            (pt1[0], pt1[1] - offset),
             cv2.FONT_HERSHEY_SIMPLEX,
-            0.9,
+            scale,
+            (0, 255, 0),
+            thickness,
+        )
+
+    for fp in fps:
+        pt1, pt2 = to_cv_pts(fp.bbox)
+        cv2.rectangle(image, pt1, pt2, (0, 0, 255), thickness)
+
+        cv2.putText(
+            image,
+            str(fp.label.name),
+            (pt1[0], pt1[1] - offset),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            scale,
             (0, 0, 255),
-            2,
+            thickness,
         )
 
 
