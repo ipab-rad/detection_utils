72c34e4ed8787126c20636a09f59f75c9c52e8d3
CTurnbull25 <callumsturnbull@hotmail.co.uk>
2024-07-20 18:29:57 +0100

    Clean up rosbag reader

--- model_evaluator/model_evaluator/readers/rosbag_reader.py
+++ model_evaluator/model_evaluator/readers/rosbag_reader.py
@@ -14,7 +14,6 @@ from model_evaluator.interfaces.dataset_reader import (
 )
 from model_evaluator.interfaces.detection2D import Detection2D
 from model_evaluator.interfaces.detection3D import Detection3D
-from model_evaluator.interfaces.labels import Label
 
 
 class RosbagReader:
@@ -29,43 +28,54 @@ class RosbagReader:
 
         # TODO: handle non mcap rosbags
 
+        self.read_rosbag_metadata(print_info)
+
+        # Create sequential reader for rosbag
+        self.reader = rosbag2_py.SequentialReader()
+
+        self.read_bag_into_reader()
+
+
+    def read_rosbag_metadata(self, print_info:bool):
         # Read rosbag metadata
         info = rosbag2_py.Info()
-        metadata = info.read_metadata(path, 'mcap')
+        metadata = info.read_metadata(self.path, 'mcap')
 
         if print_info:
-            print(f'Rosbag \'{path}\'')
+            print(f'Rosbag \'{self.path}\'')
             print(
                 f'Size: {metadata.bag_size / 1024.0 / 1024.0:.2f} MB, Duration: {metadata.duration}, Message count: {metadata.message_count}'
             )
 
         # Check wanted topics exist in rosbag and types match
-        for topic_info in metadata.topics_with_message_count:
-            if topic_info.topic_metadata.name in self.topics_with_types.keys():
-                try:
-                    topic_type = get_message(topic_info.topic_metadata.type)
+        matching_topics = [topic_info for topic_info in metadata.topics_with_message_count
+                           if topic_info.topic_metadata.name in self.topics_with_types.keys()]
 
-                    if (
-                        topic_type
-                        != self.topics_with_types[
-                            topic_info.topic_metadata.name
-                        ]
-                    ):
-                        raise TypeError(
-                            f"Topic '{topic_info.topic_metadata.name}' has different type '{topic_type}' ('{topic_info.topic_metadata.type}') than desired type '{self.topics_with_types[topic_info.topic_metadata.name]}'."
-                        )
+        for topic_info in matching_topics:
+            self.check_ros_topic_type(topic_info, metadata, print_info)
 
-                    if print_info:
-                        print(
-                            f"Topic '{topic_info.topic_metadata.name}' ('{topic_type}'): {topic_info.message_count} messages ({topic_info.message_count / metadata.duration.total_seconds():.2f} Hz)"
-                        )
-                except ModuleNotFoundError as e:
-                    raise TypeError(
-                        f"Type '{topic_info.topic_metadata.type}' for Topic '{topic_info.topic_metadata.name}' not found: {e.msg}"
-                    )
 
-        # Create sequential reader for rosbag
-        self.reader = rosbag2_py.SequentialReader()
+    def check_ros_topic_type(self,topic_info, metadata, print_info):
+        try:
+            topic_type = get_message(topic_info.topic_metadata.type)
+            expected_type = self.topics_with_types[topic_info.topic_metadata.name]
+
+            if topic_type != expected_type:
+                error_msg = f"Topic '{topic_info.topic_metadata.name}' has different type '{topic_type}' ('{topic_info.topic_metadata.type}')"
+                error_msg += f" than desired type '{self.topics_with_types[topic_info.topic_metadata.name]}'."
+                raise TypeError(error_msg)
+
+            if print_info:
+                info_msg = f"Topic '{topic_info.topic_metadata.name}' ('{topic_type}'): {topic_info.message_count} messages"
+                info_msg += f" ({topic_info.message_count / metadata.duration.total_seconds():.2f} Hz)"
+                print(info_msg)
+
+        except ModuleNotFoundError as e:
+            raise TypeError(
+                f"Type '{topic_info.topic_metadata.type}' for Topic '{topic_info.topic_metadata.name}' not found: {e.msg}"
+            )
+
+    def read_bag_into_reader(self):
         self.reader.open(
             rosbag2_py.StorageOptions(uri=self.path, storage_id='mcap'),
             rosbag2_py.ConverterOptions(
@@ -76,7 +86,7 @@ class RosbagReader:
 
         # Set filter for wanted topics only
         self.reader.set_filter(
-            rosbag2_py.StorageFilter(topics=list(topics_with_type.keys()))
+            rosbag2_py.StorageFilter(topics=list(self.topics_with_types.keys()))
         )
 
     def __iter__(self) -> Generator[tuple[str, bytes, time], None, None]:
