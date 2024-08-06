c4807108b62c27c002f64bd43ad2c31d3200c631
Julius Schulte <jschulte.oedt@t-online.de>
2024-08-06 11:28:58 +0100

    Changed to camera evaluation

--- model_evaluator/model_evaluator/model_evaluator.py
+++ model_evaluator/model_evaluator/model_evaluator.py
@@ -2,6 +2,6 @@ from model_evaluator.run_types.camera_run import camera_run
 from model_evaluator.run_types.lidar_run import lidar_run
 
 def main():
-    # camera_run()
+    camera_run()
 
-    lidar_run()
+    # lidar_run()
